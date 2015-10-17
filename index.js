5d78e7264031f7a66c51d1def5c5ed4cd5b3ceca
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-10-18 11:02:26 +1300

    basic virtual dom implemented, d3 package added

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("running");
+var h = require('virtual-dom/h');
+var diff = require('virtual-dom/diff');
+var patch = require('virtual-dom/patch');
+var createElement = require('virtual-dom/create-element');
+
+function render(count)  {
+    return h('div', [String(count)]);
+}
+
+var count = 0;
+
+var tree = render(count);
+var rootNode = createElement(tree);
+document.body.appendChild(rootNode);
+
+//updating
+// setInterval(function () {
+//       count++;
+//
+//       var newTree = render(count);
+//       var patches = diff(tree, newTree);
+//       rootNode = patch(rootNode, patches);
+//       tree = newTree;
+// }, 1000);
